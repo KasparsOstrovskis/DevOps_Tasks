300be411d227d202261ef2412f25f819a9895ce3
Ostrovskis <kaspars.ostrovskis@accenture.com>
2018-11-29 17:01:53 +0200

    finished first task

---
+++ Task1/Task1Script.ps1
@@ -0,0 +1,17 @@
+﻿$cd                        = [xml](Get-Content C:\Users\kaspars.ostrovskis\Desktop\DevOpsTasks\Task1\OriginalXML2.xml)
+$saveLocation              = "C:\Users\kaspars.ostrovskis\Desktop\DevOpsTasks\Task1\testXML.xml"
+
+$numberToRepeat            = 1..10
+
+foreach ($number in $numberToRepeat) {
+       
+$cd.note.to = "This XML for $number"
+
+$pathToSAve = "C:\Users\kaspars.ostrovskis\Desktop\DevOpsTasks\Task1\testXML$number.xml"
+$cd.Save($pathToSAve) 
+
+}
+        
+
+
+
