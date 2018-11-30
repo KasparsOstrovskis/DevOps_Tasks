9e0cf97c7cefab8db6793e8ee067c72631df1d7e
Ostrovskis <kaspars.ostrovskis@accenture.com>
2018-11-30 10:43:02 +0200

    finished second task

---
+++ Task2/Task2Script.ps1
@@ -0,0 +1,4 @@
+﻿$json   = Get-Content C:\Users\kaspars.ostrovskis\Desktop\DevOpsTasks\Task2\task2.json | ConvertFrom-Json
+$sortas = $Json.glossary.GlossDiv.GlossList.GlossEntry.SortAs = "OMPL"
+$json |ConvertTo-Json -Depth 20| Set-Content C:\Users\kaspars.ostrovskis\Desktop\DevOpsTasks\Task2\task2_saved.json
+
