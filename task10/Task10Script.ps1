5bb1486af7b78745ac30ee1ca61e6a5ee5ae0b10
Ostrovskis <kaspars.ostrovskis@accenture.com>
2018-12-03 16:06:34 +0200

    finished task 10

---
+++ task10/Task10Script.ps1
@@ -0,0 +1,15 @@
+﻿$os   =Get-Wmiobject -ComputerName localhost -Class Win32_OperatingSystem
+$hdd  =Get-WmiObject -ComputerName localhost -class Win32_LogicalDisk -filter "DeviceID='c:'"
+$Prop =[ordered]@{
+        'Organization' =$os.Organization;
+        'ComputerName'=$env:COMPUTERNAME;
+        'OS Name'=$os.caption;
+        'OS Version'=$os.version;
+        'Size(gb)'=$hdd.Size / 1gb -as [int]
+        'FreeSpace(gb)'=$hdd.freespace / 1gb -as [int]
+        'Used hdd space(gb)'=($hdd.size-$hdd.FreeSpace) /1gb -as [int]
+       }
+
+$Obj  =New-Object -TypeName PSObject -Property $Prop 
+Write-Output $Obj
+
