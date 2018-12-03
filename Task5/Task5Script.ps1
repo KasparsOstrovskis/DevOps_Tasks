761fa76c17044883d591dd5d0e3370a00595c8e0
Ostrovskis <kaspars.ostrovskis@accenture.com>
2018-12-03 12:54:13 +0200

    finished fifth task

---
+++ Task5/Task5Script.ps1
@@ -0,0 +1,17 @@
+﻿
+
+
+if(!(Test-Path variable:global:testVar))
+{
+ 
+ Write-Warning "Variable does not exist"
+ $testVar= Read-Host 'What value you want to set to $testvar?'
+
+}
+
+elseif(Test-Path variable:global:testVar)
+{
+
+Write-Output $testVar
+
+}
